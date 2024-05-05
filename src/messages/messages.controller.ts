aa96fa3ce3396fb07bdea919faf357e9f90de3b1
Vedant Mahajan <vedant.mahajan@khatabook.com>
2024-05-05 21:43:30 +0530

    initial commit for app setup + messages controller

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // Don't do this on REAL APPS
+    // USE DEPENDENCY INJECTION
+    this.messagesService = new MessagesService();
+  }
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
+}
